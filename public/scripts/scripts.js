cf2a5c75378e19f64801a27b25b7ae053dfd54c5
Renato <renatoxavier.rx@hotmail.com>
2022-04-19 08:02:44 -0300

    Active menu logic

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,20 +1,16 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
-
-for(let card of cards) {
+/* const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card'); */
+/* for(let card of cards) {
     card.addEventListener("click", function() {
-        
         /* modalOverlay.classList.add('active'); */
         //pegar o atributo do video de cada video para rodar
-        const videoId = card.getAttribute("id");
+        /* const videoId = card.getAttribute("id");
         // redirecionar o card para a pagina que deseja
         window.location.href = `/video?id=${videoId}`;
         //pegar o iframe e colocar os videos conforme os grids
         /* modalOverlay.querySelector("iframe").src= `https://www.youtube.com/embed/${videoId}`; */
-
-
-    });
-};
+/*     });
+}; */
 
 /* document.querySelector(".close-modal").addEventListener("click", function() {
     modalOverlay.classList.remove("active");
@@ -22,5 +18,15 @@ for(let card of cards) {
     //assim o video vai parar de rodar quando fechar o modal
     modalOverlay.querySelector("iframe").src ="";
 }); */
+
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
+//Use Includes Method - to include strings
+for (item of menuItems) {
+    if(currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
+}
+
 
 
